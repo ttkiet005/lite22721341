3b7dab85221c095a6d55c5cef736dcb5b98cfe9e
ismart <142441680+ismartio@users.noreply.github.com>
2024-03-29 23:25:46 +0700

    Add files via upload

---
+++ start.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const { query } = require("./data.js");
+
+app.use(express.json());
+
+app.get("/api/books", async (req, res) => {
+  console.info(`Getting all books`);
+
+  const books = await query("SELECT * FROM tblbook");
+  res.json(books);
+});
+
+app.listen(80, () => {
+  console.info(`Server started listening on port 80`);
+});
