a4ec4091f62116ef40bc22ec0ead385918a80035
iuh22721341 <157261595+ttkiet005@users.noreply.github.com>
2024-03-31 09:29:36 +0700

    Update start.js

--- start.js
+++ start.js
@@ -5,7 +5,7 @@ const { query } = require("./data.js");
 
 app.use(express.json());
 
-app.get("/api/books", async (req, res) => {
+app.get("/api22721341/books", async (req, res) => {
   console.info(`Getting all books`);
 
   const books = await query("SELECT * FROM tblbook");
